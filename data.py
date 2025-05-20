77570fdd151f1768c3066d1fea3f5fd7e4874d2d
Katherine Rodriguez <angie.kt.rodriguez23@gmail.com>
2025-05-19 20:57:08 -0500

    Se actualizo el archivo de data.py y se organizo clasificando la informacion contenida

--- data.py
+++ data.py
@@ -1,6 +1,11 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+# Configuración de la URL y rutas
+urban_routes_url = 'https://cnt-9ff0a88d-5f22-47ba-8d57-e652c1ec7b27.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
+# Datos del usuario
 phone_number = '+1 123 123 12 12'
 card_number, card_code = '1234 5678 9100', '111'
+# Preferencias del viaje
 message_for_driver = 'Muéstrame el camino al museo'
+# Cantidad de helados a pedir
+ice_cream_count = 2
