d9a3f3ea5b259b97fa61f800cba9780bebb6593a
Katherine Rodriguez <angie.kt.rodriguez23@gmail.com>
2025-05-25 23:14:23 -0500

    Se optimizo el test

--- test_urban_routes.py
+++ test_urban_routes.py
@@ -1,3 +1,4 @@
+from time import sleep
 from phone_code import retrieve_phone_code
 from  data import urban_routes_url, address_from, address_to, phone_number, message_for_driver
 from urban_routes_page import UrbanRoutesPage
@@ -26,6 +27,7 @@ class TestUrbanRoutes:
 # Definir punto de partida y punto de llegada
 
     def test_set_route(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         assert self.routes_page.get_from() == address_from
@@ -34,6 +36,7 @@ class TestUrbanRoutes:
 # Seleccionar la tarifa Comfort.
 
     def test_order_taxi_and_select_comfort_rate(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         self.routes_page.click_request_taxi()
@@ -44,27 +47,13 @@ class TestUrbanRoutes:
         classes = comfort_tariff.get_attribute("class")
         assert "tcard" in classes and ("selected" in classes or "active" in classes)
 
-# Rellenar el número de teléfono.
-
-    def test_fill_phone_number(self):
-        self.routes_page.set_from(address_from)
-        self.routes_page.set_to(address_to)
-        self.routes_page.click_request_taxi()
-        self.routes_page.select_comfort_tariff()
-
-        self.routes_page.click_phone_button()
-        self.routes_page.set_phone_number(phone_number)
-
-        phone_field = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.routes_page.phone_input))
-        assert phone_number[-5:] in phone_field.get_attribute("value")
-
 # Verificar Número de telefono
 
     def test_phone_verification(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         self.routes_page.click_request_taxi()
-        self.routes_page.select_comfort_tariff()
 
         self.routes_page.click_phone_button()
         self.routes_page.set_phone_number(phone_number)
@@ -78,21 +67,13 @@ class TestUrbanRoutes:
         assert self.routes_page.is_phone_number_displayed(
             "+1 123 123 12 12"), "El número de teléfono no se muestra correctamente en la pantalla."
 
-# Agregar medio de mapo "tarjeta de credito"
+    # Agregar medio de mapo "tarjeta de credito"
 
     def test_add_credit_card(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         self.routes_page.click_request_taxi()
-        self.routes_page.select_comfort_tariff()
-
-        self.routes_page.click_phone_button()
-        self.routes_page.set_phone_number(phone_number)
-        self.routes_page.submit_phone_number()
-
-        code = retrieve_phone_code(self.driver)
-        self.routes_page.enter_verification_code(code)
-        self.routes_page.confirm_code()
 
         self.routes_page.add_credit_card()
 
@@ -101,10 +82,10 @@ class TestUrbanRoutes:
 
 # Escribir un mensaje para el controlador.
     def test_message_driver(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         self.routes_page.click_request_taxi()
-        self.routes_page.select_comfort_tariff()
 
         self.routes_page.driver_message()
 
@@ -113,7 +94,9 @@ class TestUrbanRoutes:
             f"El mensaje no coincidió. Esperado: '{message_for_driver}', Actual: '{message_input.get_attribute('value')}'"
 
 # Pedir una manta y pañuelos.
+
     def test_ask_for_blankets_and_handkerchiefs(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         self.routes_page.click_request_taxi()
@@ -121,7 +104,9 @@ class TestUrbanRoutes:
         self.routes_page.ask_blanket_and_handkerchiefs()
 
 # Pedir 2 helados
+
     def test_order_two_ice_creams(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         self.routes_page.click_request_taxi()
@@ -137,26 +122,20 @@ class TestUrbanRoutes:
         assert value == "2", f"Se esperaba 2 helados, pero se encontró: {value}"
 
 # Llamar al taxi
+
     def test_call_taxi(self):
+        self.driver.get(urban_routes_url)
         self.routes_page.set_from(address_from)
         self.routes_page.set_to(address_to)
         self.routes_page.click_request_taxi()
         self.routes_page.select_comfort_tariff()
-        self.routes_page.click_phone_button()
-        self.routes_page.set_phone_number(phone_number)
-        self.routes_page.submit_phone_number()
-        code = retrieve_phone_code(self.driver)
-        self.routes_page.enter_verification_code(code)
-        self.routes_page.confirm_code()
-        self.routes_page.add_credit_card()
         self.routes_page.driver_message()
-        self.routes_page.ask_blanket_and_handkerchiefs()
-        self.routes_page.order_ice_cream()
-        self.routes_page.click_order_a_taxi()
+        self.routes_page.book_a_taxi()
         WebDriverWait(self.driver, 40).until(
             expected_conditions.visibility_of_element_located(self.routes_page.confirmation_information_driver)
         )
         assert self.driver.find_element(*self.routes_page.confirmation_information_driver).is_displayed()
+        sleep(5)
 
 
 
